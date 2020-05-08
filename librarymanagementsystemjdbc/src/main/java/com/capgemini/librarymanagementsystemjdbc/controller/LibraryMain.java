03ca0c517d22bba1d3a535fce0da56a43eb8302e
Bhavanineella <bhavanineella2@gmail.com>
2020-05-08 16:24:12 +0530

    first commit

--- librarymanagementsystemjdbc/src/main/java/com/capgemini/librarymanagementsystemjdbc/controller/LibraryMain.java
+++ librarymanagementsystemjdbc/src/main/java/com/capgemini/librarymanagementsystemjdbc/controller/LibraryMain.java
@@ -4,6 +4,6 @@ package com.capgemini.librarymanagementsystemjdbc.controller;
 
 public class LibraryMain {
 	public static void main(String[] args) {
-		LMSMain.doReg();
+		LMSMain.LibraryOperations();
 	}
 }
